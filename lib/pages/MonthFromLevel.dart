a916e68d55968a2b0ae7d3ef0914f3835c1d15cd
Vlad <kvitvladyslav0@gmail.com>
2024-11-03 19:53:52 +0200

    'MonthFromLevel' page realized

--- lib/pages/MonthFromLevel.dart
+++ lib/pages/MonthFromLevel.dart
@@ -15,15 +15,15 @@ class _MonthFromLevel extends State<MonthFromLevel> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.yellow,
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text(
           'Розрахування місяця\nза заданим рівнем',
           style: TextStyle(
-            fontFamily: 'Times_New_Roman',
+            fontFamily: 'Tw Cen',
           ),
         ),
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.lightBlueAccent,
         centerTitle: true,
       ),
       body: Column(
@@ -31,13 +31,13 @@ class _MonthFromLevel extends State<MonthFromLevel> {
           const Text(
             'Введiть рівень досвіду (ліворуч зверху)',
             style: TextStyle(
-              fontFamily: 'Times_New_Roman',
+              fontFamily: 'Tw Cen',
             ),
           ),
           TextField(
             onChanged: (String lvl) {
               setState(() {
-                level = int.tryParse(lvl) ?? 1000;
+                level = int.tryParse(lvl) ?? 1;
               });
             },
           ),
@@ -46,7 +46,12 @@ class _MonthFromLevel extends State<MonthFromLevel> {
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.blueAccent,
         onPressed: () {
-          year = (level ~/ 72) + 1001; // Обчислення року
+          if ((level % 72) == 0) {
+            year = (level ~/ 72) + 2024;
+            month = 'Грудень';
+          } else {
+            year = (level ~/ 72) + 2025;
+          }
 
           int levelInYear = level % 72;
           int monthIndex = (levelInYear ~/ 6) + 1;
@@ -101,7 +106,7 @@ class _MonthFromLevel extends State<MonthFromLevel> {
                 title: const Text(
                   'Місяць, що відповідає рівню',
                   style: TextStyle(
-                    fontFamily: 'Times_New_Roman',
+                    fontFamily: 'Tw Cen',
                   ),
                 ),
                 content: Text('$month $year'),
@@ -113,7 +118,7 @@ class _MonthFromLevel extends State<MonthFromLevel> {
                     child: const Text(
                       'Закрити',
                       style: TextStyle(
-                        fontFamily: 'Times_New_Roman',
+                        fontFamily: 'Tw Cen',
                       ),
                     ),
                   ),
